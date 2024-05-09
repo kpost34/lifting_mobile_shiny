31aeaa34cb2bf0d660a61fca02b42a9e35227a0f
Keith <kpost342005@hotmail.com>
2024-05-08 20:36:40 -0600

    Created separate main and workout pages/tabs

--- modules/01_main.R
+++ modules/01_main.R
@@ -1,84 +1,35 @@
-# Login Module
+# Main Module
 
 
 # UI================================================================================================
-loginUI <- function(id) {
+mainUI <- function(id) {
   ns <- NS(id)
   
-  f7Tabs(id=ns("main_pages"),
-    #login page
-    f7Tab(title="Login",
-          tabName="login_tab",
-          active=TRUE,
-          hidden=TRUE,
-      #enter username
-      f7Text(
-        inputId=ns("txt_user"),
-        label="Please enter username (min 5 characters)"
-      ),
-      #select whether returning user
-      f7Radio(
-        inputId=ns("rad_login"),
-        label="Are you a returning user?",
-        choices=c("no", "yes"),
-        selected="no"
-      ),
-      #submit username
-      f7Button(
-        inputId=ns("btn_login"),
-        label="Submit"
-      )
-    ),
-    
-    #landing page
-    f7Tab(title="Lifting Mobile App",
-            tabName="landing_tab",
-            hidden=TRUE,
-            active=FALSE,
-      h4("We are now on the landing page.")
-    )
+  #main page
+  f7Tab(title="Lifting Mobile App",
+        tabName="tab1",
+        hidden=TRUE,
+        active=FALSE,
+    h4("We are now on the main page.")
   )
-    
   
 }
 
 
-
-
-
 # Server============================================================================================
-loginServer <- function(id) {
+mainServer <- function(id) {
   moduleServer(id, function(input, ouptut, session) {
   
-    ns <- session$ns 
-    
-    #connect to google sheets and pull all user_ids
-    user_ids <- c("keith22")
-    
-    observeEvent(input$btn_login, {
-      # if(length(input$txt_user) < 5) {
-      #   print("username must be at least 5 characters long")
-      # }
-      
-      #requirements
-      # req(any(input$txt_user %in% user_ids)) & req(input$rad_login == "Yes")
-      # req(input$rad_login == "yes", any(input$txt_user %in% user_ids))
-      req((input$rad_login == "yes" & any(input$txt_user %in% user_ids))|(input$rad_login== "No" & length(input$txt_user) >= 5))
-      
-      #move to landing page
-      updateF7Tabs(id="main_pages", selected="landing_tab")
-      
-      #grab data if in database
-      # if(input$rad_login=="Yes") {
-      #   #pull data from db
-      #   # df_user <- 
-      # }
-    
-    })
+    # ns <- session$ns 
   
   })
   
 }
+
+
+
+
+
 
 
 
--- modules/02_workout.R
+++ modules/02_workout.R
@@ -1,35 +1,16 @@
-# Login Module
+# Workout Module
 
 
 # UI================================================================================================
 workoutUI <- function(id) {
   ns <- NS(id)
   
-  f7Tab(title="Login",
-        tabName="login_tab",
+  f7Tab(title="Start new workout",
+        tabName="workout_tab",
+        icon=f7Icon("arrowtriangle_right_fill"),
         active=TRUE,
-        hidden=TRUE,
-    #enter username
-    f7Text(
-      inputId=ns("txt_user"),
-      label="Please enter username (min 5 characters)"
-    ),
-    #select whether returning user
-    f7Radio(
-      inputId=ns("rad_login"),
-      label="Are you a returning user?",
-      choices=c("no", "yes"),
-      selected="no"
-    ),
-    #submit username
-    f7Button(
-      inputId=ns("btn_login"),
-      label="Submit"
-    )
-    )
+    h4("We are on the start new workout page")
   )
-    
-  
 }
 
 
