4e42f947597b5597bc3588040ee711fde258a49c
Keith <kpost342005@hotmail.com>
2024-05-08 20:37:20 -0600

    Updated app script such that user can navigate tabs in separate modules

--- app.R
+++ app.R
@@ -9,7 +9,9 @@ pacman::p_load(shiny, shinyMobile, here, tidyverse)
 
 
 ## Source files
+source(here("modules", "00_login.R"))
 source(here("modules", "01_main.R"))
+source(here("modules", "02_workout.R"))
 
 
 # App===============================================================================================
@@ -21,17 +23,19 @@ liftingApp <- function() {
       navbar=f7Navbar(
         title="Tab layout title"
       ),
+      f7Tabs(id="tabs",
       #login
-      loginUI("user"),
+      loginUI("tab0"),
       
       # #main menu
-      # mainUI("main"),
+      mainUI("tab1"),
       # 
       # #new workout
-      workoutUI("lift")
+      workoutUI("tab2")
       # 
       # #new meso
       # mesoUI("prog")
+      )
     )
   )
   
@@ -39,13 +43,18 @@ liftingApp <- function() {
   server <- function(input, output, session) {
 
     #login
-    loginServer("user")
+    tab0val <-loginServer("tab0")
     
-    # #main menu
-    # mainServer("main")
-    # 
+    #main menu
+    mainServer("tab1")
+    
+    
+    observeEvent(tab0val(), {
+      updateF7Tabs(id='tabs', selected='tab1')
+    },ignoreInit=TRUE)
+    
     # #new workout
-    # workoutServer("lift")
+    workoutServer("lift")
     # 
     # #new meso
     # mesoServer("prog")
